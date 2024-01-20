2438669fd8b54e2c63605cdd9c363a08ba3a7438
Daria Kokot <dariakokot@student.agh.edu.pl>
2024-01-20 13:50:42 +0100

    Recognizer implemented in UI

--- Projekt_nnLettersRecognizer/nn_textRecognizer.m
+++ Projekt_nnLettersRecognizer/nn_textRecognizer.m
@@ -23,13 +23,22 @@ imgDatastore.Labels = categorical(cellfun(@(filename) extractLabel(filename), im
 % losowy
 [trainData, valData, tData] = splitEachLabel(imgDatastore, 0.8, 0.1, 0.1, 'randomized');
 
+%Augmentacja
+% Specify data augmentation options
+augmenter = imageDataAugmenter('RandXReflection',true,'RandRotation',[-5,5]);
+
+% Create augmented datastores
+augmentedTrainData = augmentedImageDatastore([32 32 1], trainData, ...
+    'DataAugmentation',augmenter);
+
 % Przygotuj dane treningowe
 % Konwersja do odcieni szarości
-imageData = cellfun(@(x) im2gray(imresize(imread(x), [32, 32])), trainData.Files, 'UniformOutput', false);
+imageData = cellfun(@(x) im2gray(imresize(imread(x), [32, 32])), augmentedTrainData.Files, 'UniformOutput', false);
 % Konwersja do tablicy 4D
 imageData = cat(4, imageData{:});
 % Narmalizacja wartości pikseli [0, 1]
 imageData = double(imageData) / 255;
+
 
 % Przygotuj dane walidacyjne (wykorzystane do ulepszania modelu podczas
 % treningu)
@@ -40,6 +49,9 @@ validationData = cat(4, validationData{:});
 % Normalize the pixel values to be in the range [0, 1]
 validationData = double(validationData) / 255;
 
+
+
+
 % Przygotuj dane testowe (wykorzystane do sprawdzenia poprawności
 % wytrenowanego modelu)
 % Konwersja do odcieni szarości
@@ -49,7 +61,9 @@ testData = cat(4, testData{:});
 % Normalize the pixel values to be in the range [0, 1]
 testData = double(testData) / 255;
 
-%%%%% ODKOMENTUJ JEŚLI CHCESZ TRENOWAĆ MODEL %%%%%
+
+
+% %%%% ODKOMENTUJ JEŚLI CHCESZ TRENOWAĆ MODEL %%%%%
 % % Dostosowanie hiperparametrów
 % options = trainingOptions('adam', ...
 %     'MaxEpochs', 50, ...
@@ -58,48 +72,59 @@ testData = double(testData) / 255;
 %     'Shuffle', 'every-epoch', ...
 %     'ValidationData', {validationData, valData.Labels}, ...
 %     'ValidationFrequency', 10, ...
-%     'Plots', 'training-progress');
-%
+%     'Plots', 'training-progress', ...
+%     'CheckpointPath', tempdir, ...
+%     'ExecutionEnvironment', 'auto', ...
+%     'Verbose', false, ...
+%     'LearnRateSchedule', 'piecewise', ...
+%     'LearnRateDropFactor', 0.1, ...
+%     'LearnRateDropPeriod', 10, ...
+%     'GradientThresholdMethod', 'absolute', ...
+%     'GradientThreshold', 0.05, ...
+%     'L2Regularization', 0.01);
+%     % 'OutputFcn', @(info, event)stopIfAccuracyNotImproving(info, event, 5)
+% 
+% 
 % % Architektura sieci
 % layers = [
 %     imageInputLayer([32 32 1])
-%
+% 
 %     convolution2dLayer(3, 16, 'Padding', 'same')
 %     reluLayer()
 %     maxPooling2dLayer(2, 'Stride', 2)
-%
+% 
 %     convolution2dLayer(3, 32, 'Padding', 'same')
 %     reluLayer()
 %     maxPooling2dLayer(2, 'Stride', 2)
-%
+% 
 %     convolution2dLayer(3, 64, 'Padding', 'same')
 %     reluLayer()
 %     maxPooling2dLayer(2, 'Stride', 2)
-%
+% 
 %     flattenLayer()
-%
+% 
 %     fullyConnectedLayer(256)
 %     reluLayer()
-%
+%         dropoutLayer(0.5)
+% 
 %     fullyConnectedLayer(52)
 %     softmaxLayer()
 %     classificationLayer()
 % ];
-%
+% 
 % % Trening sieci
 % net = trainNetwork(imageData, trainData.Labels, layers, options);
-%
+% 
 % % Zapisz wytrenowany model
-% save('trainedModel.mat', 'net');
-% return;
+% save('trainedModel_v2_dropout.mat', 'net');
+
 
 
 %%%%% ODKOMENTUJ JEŚLI CHCESZ TRENOWAĆ MODEL - KONIEC %%%%%
 
-
 %%%%% SPRAWDŹ POPRAWNOŚĆ MODELU %%%%%
 % Wczytaj wytrenowany model
-load('trainedModel.mat', 'net');
+load('trainedModel_v2_dropout.mat', 'net');
 tData.Labels = categorical(tData.Labels);
 
 % Wykorzystaj wytrenowany model do określenia etykiet dla zbiory testowego
@@ -125,5 +150,22 @@ else
     label = name(1:underscoreIndex(1)-1);
 end
 end
+
+% Zatrzymaj trening jeśli accuracy się już nie poprawia
+% function stop = stopIfAccuracyNotImproving(info, event, threshold)
+% 
+%     stop = false;
+% 
+%     Czy accuracy w walidacji się nie poprawiło
+%     if info.State.Iteration > 1
+%         currentAccuracy = info.ValidationAccuracy(end);
+%         previousAccuracy = info.ValidationAccuracy(end - 1);
+% 
+%         Stop
+%         if currentAccuracy <= previousAccuracy + threshold
+%             stop = true;
+%         end
+%     end
+% end
 
 %-----------------------------------
--- Recognizer/TextRecognizer.m
+++ Recognizer/TextRecognizer.m
@@ -1,29 +1,33 @@
 classdef TextRecognizer
     methods
-        function resultText = recognize(obj, someInput, recognizerValues)
-
-            load('trainedModel.mat', 'net');
-
-            % Wczytaj obrazek - to tylko przykład dla jednego obrazka. My
-            % powinniśmy dostać jako input tablicę z macierzami
-            % reprezenującymi obrazki!!!
-
-            imagePath = 'testImageA.png';
-            inputImage = imread(imagePath);
-            % Convert the image to grayscale
-            inputImage = rgb2gray(inputImage);
-            
-            % Preprocess the image
-            inputImage = imresize(inputImage, [32, 32]);
-            inputImage = im2double(inputImage);
-            
-            % Ensure the image has the correct dimensions
-            inputImage = reshape(inputImage, [32, 32, 1]);
-
-            predictedLabel = classify(net, inputImage);
-            textArray = char(str2double(char (predictedLabel)));
-            
-            % define return value
+        function resultText = recognize(obj, paragraphs, recognizerValues)
+            textArray="";
+            for j = 1:numel(paragraphs)
+                images=paragraphs{j}.images;
+                flags=paragraphs{j}.flags;
+    
+                % disp(images);
+                % load("trainedModel_11_epochs_advanced.mat", 'net');
+                load('trainedModel.mat', 'net');
+    
+                for i = 1:numel(images)
+                    if (flags(i))
+                        textArray = strcat(textArray, " ");
+                    end
+                    inputImage = images{i};
+    
+                    % Upewnij się czy wymiary są poprawne
+                    inputImage = reshape(inputImage, [32, 32, 1]);
+    
+                    predictedLabel = classify(net, inputImage);
+                    predictedChar = char(str2double(char (predictedLabel)));
+                    textArray = strcat(textArray, predictedChar);
+                    % disp(flags(i));
+                       
+                end
+                textArray = strcat(textArray, newline);
+            end
+            % Zwróć wynik
             resultText = textArray;
         end
     end
@@ -33,26 +37,26 @@ end
 % classdef TextRecognizer
 %     methods
 %         function resultText = recognize(obj, someInput, recognizerValues)
-% 
+%
 %             % if something goes wrong you'll get zeros
 %             % in the recognizerValues cell array (but it shouldn't lol)
-% 
+%
 %             % for now it'll be easier for you to edit what you get if you
 %             % get a whole cell array, but once you tell me exactly what fields
 %             % you'd like to have, then you won't have to map like this:
-% 
+%
 %             slider3_value = recognizerValues{1};
 %             slider4_value = recognizerValues{2};
 %             editfield_value = recognizerValues{3};
 %             doubleslider_value_min = recognizerValues{4}(1);
 %             doubleslider_value_max = recognizerValues{4}(2);
-% 
-%             % idk what input you'll get from the separator, sorry 
+%
+%             % idk what input you'll get from the separator, sorry
 %             someInput;
-% 
-% 
+%
+%
 %             textArray = {'this is ' '   some text' '' 'mind the lack of commas in matlab arrays 😒'};
-% 
+%
 %             % define return value
 %             resultText = textArray;
 %         end
